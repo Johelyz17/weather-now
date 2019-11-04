3e9702ca5f0a14a034b643bc8da62cdbff5c1032
Johelzy17 <Johelyz15@icloud.com>
2019-11-04 11:17:28 -0500

    commit all file

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
  
+// settting view engine
+app.set("view engien", "ejs")
+
+/* 
+ROUTES
+*/
+
+// get /
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.render("home.ejs");
+});
  
 app.listen(3000,function(){
-    console.log("ser")
-})
+    console.log("server is live on port:3000");
+});
